35e9941bf54db66118fc6d4e2d752cbb9bdb63b6
taufiqdp <topexdw@gmail.com>
2024-09-07 20:51:32 +0700

    update: main header, skills, social media as components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import MainHeader from "@/components/main-header";
+import Skills from "@/components/skills";
+import SocialMedia from "@/components/social-media";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-24">
+    <main className="flex flex-col gap-32">
       <MainHeader />
+      <Skills />
+      <SocialMedia />
     </main>
   );
 }
