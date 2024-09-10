c1d8726a4a87d6ab39e510a83d9ee29483ed5d52
taufiqdp <topexdw@gmail.com>
2024-09-10 09:40:22 +0700

    update: add education section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
+import Education from "@/components/education";
 import MainHeader from "@/components/main-header";
 import Skills from "@/components/skills";
 import SocialMedia from "@/components/social-media";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-32">
+    <main className="flex flex-col">
       <MainHeader />
       <Skills />
+      <Education />
       <SocialMedia />
     </main>
   );
