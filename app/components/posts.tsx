fa2dbce0e92a685ef642701b09c7078fa525e71f
taufiqdp <topexdw@gmail.com>
2025-02-10 08:57:09 +0700

    style: blog post layout and text visibility

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -22,13 +22,14 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 w-[128px] tabular-nums">
+            <div className="w-full flex flex-col space-y-4 -mx-2 md:-mx-4 p-2 md:p-4 hover:bg-gray-100 rounded-2xl">
+              <p className="text-neutral-500 tabular-nums border-l-3 rounded-sm border-l-neutral-400 pl-2">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-900 tracking-tight hover:underline hover:underline-offset-2">
+              <p className="text-neutral-900 tracking-tight font-semibold hover:underline hover:underline-offset-2">
                 {post.metadata.title}
               </p>
+              <p className="text-neutral-600">{post.metadata.summary}</p>
             </div>
           </Link>
         ))}
